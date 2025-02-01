ca39e0ae1dce2f86cfc83dcdb8db2fe4d9683498
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-02-01 19:12:31 +0530

    changes

--- backend/doctors/urls.py
+++ backend/doctors/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
+from .import views
 from .views import DoctorDetailView, DoctorRegisterView
 
 
 
 urlpatterns = [
+    path('api/doctor/list/', views.get_all_doctors, name="get-all-doctors"),
+    path('api/admin/change-availibility/', views.change_availibity, name="change-availibility"),
+
     path('doctors/<int:pk>/', DoctorDetailView.as_view(), name='doctor-detail'),
     path('doctors/register-doctor/', DoctorRegisterView.as_view(), name='doctor-register'),
+
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
--- backend/doctors/views.py
+++ backend/doctors/views.py
@@ -1,9 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+
 from .models import Doctor
-from .serializers import DoctorSerializer, DoctorRegisterSerializer
+from .serializers import DoctorSerializer, DoctorRegisterSerializer, GetAllDoctorsSerializer
 
 class DoctorDetailView(generics.RetrieveUpdateDestroyAPIView):
- 
+    permission_classes = [IsAuthenticated]
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
 
@@ -12,4 +17,31 @@ class DoctorRegisterView(generics.CreateAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorRegisterSerializer
 
- 
+@api_view(['GET'])
+def get_all_doctors(request):
+    doctors = Doctor.objects.all()
+    serializer = GetAllDoctorsSerializer(doctors, many=True)
+    data = {
+        "success": "true",
+        "doctors": serializer.data
+    }
+    return Response(data)
+
+@api_view(['POST'])
+def change_availibity(request):
+    doc_id = request.data.get('docId')
+    try:
+        doctor = Doctor.objects.get(id=doc_id)  # Find doctor
+        doctor.available = not doctor.available
+        doctor.save()  # Save changes
+    
+    except Doctor.DoesNotExist:
+        return Response({
+            "sucess": "false",
+            "error": "Doctor not found"
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({
+        "success": "true",
+        "message": "availibility updated successfully"
+    })
