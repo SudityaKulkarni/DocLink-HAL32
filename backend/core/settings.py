ef61fbe3aa8e4b92e0d6f4d6c054afeda1d6dfe3
SudityaKulkarni <suditya.kulkarni@gmail.com>
2025-02-01 22:44:48 +0530

    added swagger UI

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_yasg',
+    'drf_spectacular',
     'doctors',
     'patients',
 
@@ -126,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS':"drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE":"Django DRF Telemedical",
+}
